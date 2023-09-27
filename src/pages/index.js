dc8265d9c7ccedb3cdbe25c1a52f8317fd40f207
Lilia <liliabendjeddou9@gmail.com>
2023-09-27 11:53:09 +0100

    feat(dark mode): add dark mode toggle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import fetcher from "@/util/API"
 import { useAppcontext } from "@/context/state"
 import { useTheme } from "next-themes"
+import AllProducts from "@/components/AllProducts/AllProducts"
+import Categories from "@/components/Categories/Categories"
 
-export default function Home({ products }) {
+export default function Home({ products, categories }) {
   const { theme, setTheme } = useTheme()
   const { darkMode, toggledarkMode } = useAppcontext()
   function handleMode() {
@@ -11,12 +13,14 @@ export default function Home({ products }) {
   }
   console.log("products", products)
   return (
-    <main className="bg-yellow-100 p-32 dark:bg-OxfordBlue flex justify-center items-center">
-      <div className="w-20 h-20">
+    <main className="bg-yellow-100 p-32 dark:bg-OxfordBlue ">
+      <div className="w-20 h-20 ">
         <button onClick={handleMode} className="bg-red-400">
           toggle
         </button>
       </div>
+      <Categories categories={categories} />
+      <AllProducts products={products} />
     </main>
   )
 }
@@ -25,8 +29,9 @@ export async function getServerSideProps() {
   const url = "products"
   try {
     const products = await fetcher(url)
+    const categories = await fetcher("products/categories")
     return {
-      props: { products },
+      props: { products, categories },
     }
   } catch (error) {
     console.log(error)
