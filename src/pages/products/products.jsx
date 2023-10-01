865b4bc257584a55e271fdb3d814515c2fe8bc90
Lilia <liliabendjeddou9@gmail.com>
2023-10-01 11:44:17 +0100

    feat(page): add all products page

--- src/pages/products/products.jsx
+++ src/pages/products/products.jsx
@@ -1,22 +1,10 @@
 import React from "react"
 import fetcher from "@/util/API"
 import ProductCard from "@/components/ProductCard/ProductCard"
+import AllProducts from "@/components/AllProducts/AllProducts"
 
 export default function Products({ products }) {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="font-bold text-3xl font-poppins ml-20">
-        Explore Our Products
-      </h1>
-      <div className="grid grid-cols-2 sm:grid-cols-4 gap-x-2 gap-y-8 m-20">
-        {products.products.map((product) => (
-          <div key={product.id}>
-            <ProductCard {...product} />
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+  return <AllProducts products={products} />
 }
 
 export async function getServerSideProps() {
