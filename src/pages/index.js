9c7e9f6e4c73084766b2149c74470f6ab36d875c
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-10-02 04:59:23 +0100

    feat(add categories carousel display): render categories in slider and fix shopping cart access
    BREAKING CHANGE: enhance home page display
    
    "closes #53"

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,14 @@ import Slider from "@/components/Slider/Slider"
 import { useEffect } from "react"
 import Link from "next/link"
 import ProductSlider from "@/components/ProductSlider/ProductSlider"
-import Categories from "@/context/data"
+import { Categories, images } from "@/context/data"
 import Services from "@/components/Services/Services"
+import CategoriesSlider from "@/components/CategoriesSlider/CategoriesSlider"
 
 export default function Home({ products, categories }) {
   const { theme, setTheme } = useTheme()
   const { darkMode, toggledarkMode, isShoppingCartOpen } = useAppcontext()
 
-  const images = [
-    "https://tinyurl.com/Echeri",
-    "https://tinyurl.com/Echeri2",
-    "https://tinyurl.com/Echeri3",
-  ]
   useEffect(() => {
     setTheme(darkMode ? "light" : "dark")
   }, [darkMode])
@@ -28,20 +24,20 @@ export default function Home({ products, categories }) {
     setTheme(darkMode ? "light" : "dark")
   }
   // ;[darkMode]
-  // console.log({ Categories })
 
   return (
     <main className="bg-DarkWhite  dark:bg-OxfordBlue ">
       <Slider images={images} />
       <ProductSlider products={products} />
-      <div className="flex items-center justify-center">
+      <CategoriesSlider />
+      {/* <div className="flex items-center justify-center">
         <img
           src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/online-classes-study-online-from-home-banner-design-template-dfe22f6485ee61ddb60257c85e4afa9b_screen.jpg?ts=1615781771"
           alt="Banner"
           className="w-120 max-h-52 object-cover item-center"
         />
-      </div>
-      {isShoppingCartOpen && <ShoppingCart />}
+      </div> */}
+      {/* {isShoppingCartOpen && <ShoppingCart />} */}
       <Services />
     </main>
   )
